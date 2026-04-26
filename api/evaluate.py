cd6154da443ff6b466842edf6f35f52ff86bf97d
yuyangjungle <yuyangj0112@gmail.com>
2026-04-26 16:52:47 +0800

    Add Vercel entrypoints for full deployment

---
+++ api/evaluate.py
@@ -0,0 +1,11 @@
+from _shared import JsonHandler
+from service_core import evaluate_prompt_versions, resolve_dataset_from_payload
+
+
+class handler(JsonHandler):
+    def do_POST(self):
+        payload = self.read_json()
+        dataset = resolve_dataset_from_payload(payload)
+        left_prompt_version = payload.get("left_prompt_version", "v1")
+        right_prompt_version = payload.get("right_prompt_version", "v2")
+        self.send_json(evaluate_prompt_versions(dataset, left_prompt_version, right_prompt_version))
